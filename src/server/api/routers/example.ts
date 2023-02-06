2c7e1100f6029ca4e9e099b416b3098afaa89d36
Matt Flowers <mattmflowers28@gmail.com>
2023-02-05 23:51:03 -0500

    Return stockfish greeting

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -2,12 +2,25 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 
+import { spawn } from "child_process";
+
+const getBestMove = () => {
+  return new Promise<string>((resolve) => {
+    const stockfish = spawn("vendor/Stockfish/src/stockfish");
+    stockfish.stdout.on("data", (data: Buffer) => {
+      console.log(data.toString());
+      stockfish.stdin.write("quit\n");
+      resolve(data.toString());
+    });
+  });
+};
+
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(async ({ input }) => {
       return {
-        greeting: `Hello ${input.text}`,
+        greeting: `Hello ${await getBestMove()}`,
       };
     }),
 });
